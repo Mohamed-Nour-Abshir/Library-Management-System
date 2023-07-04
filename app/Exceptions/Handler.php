12898962ee396fd3b94799dcaf2fad80d702bb14
Mohamed-Nour-Abshir <mnor32779@gmail.com>
2023-07-04 14:02:00 +0600

    initial commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    protected $dontReport = [
+        //
+    ];
+
+
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+    
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    // Other methods and code...
+    public function render($request, Throwable $exception)
+    {
+        return parent::render($request, $exception);
+    }
+}
---
+++ resources/views/layout/template_footer.blade.php
@@ -0,0 +1,5 @@
+;<div class="footer" style="background-color: #00cc00;">
+	<div class="container">
+		<center><b class="copyright" style="color: white;">&copy; {{ date('Y') }} - DIU Library!...All rights reserved. </b> </center>
+	</div>
+</div>
